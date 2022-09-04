5ab36e3027b5567239256f3ab8dff43f6e4444bf
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2022-09-04 13:08:43 +0600

    google font added star stop reset button funtioned successfully star stop and reset function added on script file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let hr = 0;
-let min = 0;
-let sec = 0;
-let ms = 0;
+let hr = "0" + 0;
+let min = "0" + 0;
+let sec = "0" + 0;
+let ms = "0" + 0;
 let startTimer;
 const startBtn = document.querySelector(".start");
 const stopBtn = document.querySelector(".stop");
@@ -20,7 +20,18 @@ function start() {
         ms = ms < 10 ? "0" + ms : ms;
         if (ms == 100) {
             sec++;
+            sec = sec < 10 ? "0" + sec : sec;
             ms = "0" + 0;
+        }
+        if (sec == 60) {
+            min++;
+            min = min < 10 ? "0" + min : min;
+            sec = "0" + 0;
+        }
+        if (min == 60) {
+            hr++;
+            hr = hr < 10 ? "0" + hr : hr;
+            min = "0" + 0;
         }
 
         putValue();
@@ -28,13 +39,19 @@ function start() {
 }
 function stop() {
     startBtn.classList.remove("active");
-    stopBtn.classList.add("stopActive")
+    stopBtn.classList.add("stopActive");
+    clearInterval(startTimer);
 }
 function reset() {
     startBtn.classList.remove("active");
-    stopBtn.classList.remove("stopActive")
+    stopBtn.classList.remove("stopActive");
+    clearInterval(startTimer);
+    hr = min = sec = ms = "0" + 0;
+    putValue();
 }
 function putValue() {
     document.querySelector(".millisecond").innerText = ms;
     document.querySelector(".second").innerText = sec;
+    document.querySelector(".minute").innerText = min;
+    document.querySelector(".hour").innerText = hr;
 }
